160218b0c58b82598450b6a3a6348bebaa9c0a94
Arnie17Poma <thulevinson@gmail.com>
2022-05-12 17:33:07 +0000

    nombre

---
+++ repetitivas/ejercicio4R.java
@@ -0,0 +1,33 @@
+package repetitivas;
+
+import java.util.Scanner;
+
+/**
+ * ejercicio4R
+ */
+public class ejercicio4R {
+      
+    
+    public static void nombrevertical() {
+        Scanner it=new Scanner(System.in);
+        System.out.println("ingrese su nombre");
+        String nombre=it.next();
+
+        for (int i = 0; i < nombre.length(); i++) {
+            System.out.println(nombre.charAt(i));
+
+        }
+        
+    }
+    
+
+
+     public static void main(String[] args) {
+
+        /*System.out.println("inprimir while"); inprimirwhileN1_10();
+        System.out.println("imprimir do while");inprimirDowhileN1_20();
+        System.out.println("inprimir for");inprimirForN1_20();*/
+
+        nombrevertical();
+     }
+}
