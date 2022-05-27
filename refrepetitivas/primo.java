e7cc73cf47528222cb31e66e60c4f1e6b0fde978
Arnie17Poma <thulevinson@gmail.com>
2022-05-27 22:02:52 +0000

    R

--- refrepetitivas/primo.java
+++ refrepetitivas/primo.java
@@ -6,31 +6,28 @@ import java.util.Scanner;
  * primo
  */
 public class primo {
-    static Scanner leerTScanner=new Scanner(System.in);
+    static Scanner leerT=new Scanner(System.in);
     public static void numeroprimo() {
-        System.out.println("ingrese el numero limite:");
-        int num=leerTScanner.nextInt();
-        int cont=1;
-        int cantDiv=0;
+        System.out.println("ingrese el limite:");
+        int num=leerT.nextInt();
         int numx=1;
         while (numx<=num) {
-            if (cantDiv==2) {
-                System.out.println("num");
+            int cantDiv=0;
+            int cont=1;      
+            while (cont<=numx) {
+                if (cantDiv>2) break;
+                if (num%cont==0) {
+                    cantDiv++;
+                }   
+                cont++;         
             }
-        }
-        while (cont<=num) {
-            if (num%cont==0) {
-                cantDiv++;
-            }   
-            cont++;         
-        }
-        if (cantDiv==2) {
-            System.out.println(+num+" es primo");
-        }else{
-            System.out.println("el numero "+num+" no es primo");
+            if (cantDiv==2) {
+                System.out.println(numx);
+            }
+            numx=numx+1;
         }
     }
-
+    
     public static void main(String[] args) {
         numeroprimo();
         
